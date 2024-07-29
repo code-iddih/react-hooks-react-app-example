5a8668133df7fa9cb0d34381a0e37c1e7c3757bc
code-iddih <idinyamwaya@gmail.com>
2024-07-29 14:05:58 +0300

    Importing TestComponent

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
         HTML, and also allows us to add in components, which are separate,
-        self-contained chunks of JSX.
+        self-contained chunk of JSX.
       </p>
       <ExampleComponent />
       <TestComponent />
